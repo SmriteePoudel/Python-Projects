1c07f89c1f015defbc703d43967e5b48f736647a
Smritee <esmritypoudel@gmail.com>
2025-04-05 11:15:29 +0530

    ChatBot.py

---
+++ ChatBot.py
@@ -0,0 +1,16 @@
+def chatbot():
+    print("ChatBot: Hello! Type 'bye' to exit.")
+    while True:
+        user_input = input("You: ").lower()
+
+        if user_input == "hello":
+            print("ChatBot: Hi there!")
+        elif user_input == "how are you":
+            print("ChatBot: I'm doing great. Thanks!")
+        elif user_input == "bye":
+            print("ChatBot: Goodbye!")
+            break
+        else:
+            print("ChatBot: Sorry, I don't understand that.")
+
+chatbot()
